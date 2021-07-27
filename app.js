e46092f757d4fd2bc6aabb04f0553ad6464c41d2
Korwin Briggs <Korwin.Briggs@gmail.com>
2021-07-27 19:40:03 -0400

    added mapbox api and basic error handling

--- app.js
+++ app.js
@@ -3,32 +3,68 @@ import axios from 'axios'
 import fetch from 'node-fetch'
 //These all do the same thing, but i'm importing both to learn both.
 //Also, asking the internet whether ot use axios or node-fetch was a four
-//hour rabbit hole that contained a lot of arrogance but no firm answers.
+//hour rabbit hole that contained a lot of posturing but no sure answers.
 
 
-const url = 'http://api.weatherstack.com/current?access_key=9dedbc55814438072235d930b8d5fa45&query=37.8267,-122.4233&units=f'
-
+const urlWeather = 'http://api.weatherstack.com/current?access_key=9dedbc55814438072235d930b8d5fa45&query=37.8267,-122.4233&units=f'
+const urlGeo = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoia29yd2luIiwiYSI6ImNrcm1iM25wMjF4MjQyb280b3VndWc1amEifQ.XG-Vj5_uZaWDNF9DSwr9HQ&limit=1'
 
 //REQUEST
 
-request( {url: url, json: true}, (err, res) => {
-    console.log(res.body.current.weather_descriptions[0]+".")
-    console.log("It is currently " + res.body.current.temperature + " degrees out.")
-    console.log("It feels like " + res.body.current.feelslike + " degrees out.")
-})
+// request( {url: urlWeather, json: true}, (err, res) => {
+//     if (err) { 
+//         console.log('Unable to connect to weather service')
+//     } else if (res.body.error) {
+//         console.log('Unable to find location')
+//     } else {
+//         console.log(res.body.current.weather_descriptions[0]+".")
+//         console.log("It is currently " + res.body.current.temperature + " degrees out.")
+//         console.log("It feels like " + res.body.current.feelslike + " degrees out.")
+//     }
+// })
+
+// request( {url: urlGeo, json: true}, (err, res) => {
+//     if (err) {
+//         console.log('Unable to connect to map service')
+//     } else if (res.body.features.length === 0) {
+//         console.log('Unable to match search term to location')
+//     } else {
+//         const latitude = res.body.features[0].center[1]
+//         const longitude = res.body.features[0].center[0]
+//         console.log("Latitude: " + latitude)
+//         console.log("Longitude: " + longitude)
+//     }
+// })
+
+
 
 //AXIOS
 
-// axios.get(url)
-// .then((response) => {
-//     console.log(response.data.current)
-// })
-// .catch((error) => {
-//     console.log(error)
-// })
+// axios.get(urlWeather)
+//     .then((response) => {
+//         if (response.data.error) {
+//             console.log('Weather service unable to find location')
+//         } else {
+//             console.log(response.data.current.weather_descriptions[0]+".")
+//             console.log("It is currently " + response.data.current.temperature + " degrees out.")
+//             console.log("It feels like " + response.data.current.feelslike + " degrees out.")
+//         }
+//     })
+//     .catch((error) => {
+//         console.log('Unable to connect to weather service')
+//     })
 
-//FETCH
-
-// fetch(url)
-//     .then(res => res.json())
-
+// axios.get(urlGeo)
+//     .then( (response) => {
+//         if (response.data.features.length === 0) {
+//             console.log('Unable to match search term to location')
+//         } else {
+//             const latitude = response.data.features[0].center[1]
+//             const longitude = response.data.features[0].center[0]
+//             console.log("Latitude: " + latitude)
+//             console.log("Longitude: " + longitude)
+//         }
+//     })
+//     .catch( (error) => {
+//         console.log('Unable to match search term to location')
+//     })
