ebce2e5e3e73a923956130952a299d92505ccad0
Korwin Briggs <Korwin.Briggs@gmail.com>
2021-08-06 20:53:51 -0400

    learning about handlebars, trying mustache stuff

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,25 +1,61 @@
 // import express from 'express'
 const express = require('express')
 const path = require('path')
-
-console.log(__dirname)
-console.log(path.join(__dirname, '../public'))
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+//define paths for express config -- public, and handlebar views
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//setup handlebars engine/views locations
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve
 app.use(express.static(publicDirectoryPath))
-//includes public/index.html, which works as home by default
 
 app.get('', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'Weather',
+        name: 'Korwin',
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Korwin',
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Korwin',
+        helpText: 'This is an example help message',
+    })
 })
 
 app.get('/weather', (req, res) => {
     res.send({
         location: 'New York',
-        weather: 'Partly weather'
+        weather: 'Partly weather',
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        errorMessage: 'help article not found lol'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        errorMessage: 'page not found lol'
     })
 })
 
